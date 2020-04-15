04085f16bfa49b080d76465a9943cc30184b44ea
Noe <noe.joaquin.james@gmail.com>
2020-04-15 14:49:40 -0400

    getting started with blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
-
-export default () => <div>Hello world!</div>
+import { Link } from "gatsby"
+export default () => (
+  <div>
+   <h1>Main Page</h1>
+   <Link to="/programming/">Posts on Programming</Link>
+  </div>
+)
---
+++ src/pages/programming.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => (
+	<div>
+		<h1>Programming Posts</h1>
+	</div>
+)
